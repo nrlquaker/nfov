003ebcf878d942c48800ce5120107a4fe733e7cf
nrlquaker <ustynov.vy@gmail.com>
2017-11-08 12:18:32 +0200

    Update dependencies

--- src/fs/storage.ts
+++ src/fs/storage.ts
@@ -1,49 +1,51 @@
 import * as settings from 'electron-settings'
 
+const options = { prettify: true }
+
 export function setBgColor(bgColor: string): void {
-    settings.set('bgColor', bgColor, { prettify: true })
+    settings.set('bgColor', bgColor, options)
 }
 
 export function getBgColor(): string {
-    return settings.get('bgColor', '#000000') as string
+    return settings.get('bgColor', '#000000', options) as string
 }
 
 export function setTextColor(textColor: string): void {
-    settings.set('textColor', textColor, { prettify: true} )
+    settings.set('textColor', textColor, options)
 }
 
 export function getTextColor(): string {
-    return settings.get('textColor', '#FFFFFF') as string
+    return settings.get('textColor', '#FFFFFF', options) as string
 }
 
 export function setLinkColor(linkColor: string): void {
-    settings.set('linkColor', linkColor, { prettify: true })
+    settings.set('linkColor', linkColor, options)
 }
 
 export function getLinkColor(): string {
-    return settings.get('linkColor', '#01FF70') as string
+    return settings.get('linkColor', '#01FF70', options) as string
 }
 
 export function setSelectionColor(selectionColor: string): void {
-    settings.set('selectionColor', selectionColor, { prettify: true })
+    settings.set('selectionColor', selectionColor, options)
 }
 
 export function getSelectionColor(): string {
-    return settings.get('selectionColor', '#7FDBFF') as string
+    return settings.get('selectionColor', '#7FDBFF', options) as string
 }
 
 export function setFontName(fontName: string): void {
-    settings.set('fontName', fontName, { prettify: true })
+    settings.set('fontName', fontName, options)
 }
 
 export function getFontName(): string {
-    return settings.get('fontName', 'Px437 IBM PS/2thin2') as string
+    return settings.get('fontName', 'Px437 IBM PS/2thin2', options) as string
 }
 
 export function setFontSize(fontSize: string): void {
-    settings.set('fontSize', fontSize, { prettify: true })
+    settings.set('fontSize', fontSize, options)
 }
 
 export function getFontSize(): string {
-    return settings.get('fontSize', '13') as string
+    return settings.get('fontSize', '13', options) as string
 }

5b39caefcdd065835431f0b365e602bea383dc24
nrlquaker <ustynov.vy@gmail.com>
2018-05-19 20:18:05 +0300

    Don't render Sauce record

--- src/fs/load-file.ts
+++ src/fs/load-file.ts
@@ -1,9 +1,29 @@
-import { readFileSync } from 'fs'
+import * as fs from 'fs'
 import { decode } from 'iconv-lite'
 import '../extensions/string'
 
 export default function loadFile(filePath: string): string {
-    const fileContent = readFileSync(filePath)
-    const data = decode(fileContent, 'cp437')
+    const fileContent = fs.readFileSync(filePath)
+    let data = decode(fileContent, 'cp437')
+    if (isSaucePresent(filePath)) {
+        data = removeSauce(data)
+    }
     return data.replaceAll('<', '&lt') // escape tags
+}
+
+// http://www.acid.org/info/sauce/sauce.htm
+function isSaucePresent(filePath: string): boolean {
+    const stats = fs.statSync(filePath)
+    const fd = fs.openSync(filePath, 'r')
+    const buffer = new Buffer(stats.size)
+    fs.readSync(fd, buffer, 0, buffer.length, stats.size - 128)
+    let id: string = ''
+    for (let i = 0; i < 5; i++) {
+        id += String.fromCharCode(buffer[i])
+    }
+    return id === 'SAUCE'
+}
+
+function removeSauce(data: string): string {
+    return data.substring(0, data.length - 128 - 1) // exclude EOF character
 }

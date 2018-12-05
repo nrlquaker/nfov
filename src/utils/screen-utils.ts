7d69ab865084887d932d22367ae56ace01ca4042
nrlquaker <ustynov.vy@gmail.com>
2018-12-05 21:44:22 +0200

    Fix document height
    For case when screenHeight > height > screen height - dock height

--- src/utils/screen-utils.ts
+++ src/utils/screen-utils.ts
@@ -4,7 +4,8 @@ export function getWindowBoundsCentered(width: number, height: number): Rectangl
     const screenWidth = screen.getPrimaryDisplay().workAreaSize.width
     const screenHeight = screen.getPrimaryDisplay().workAreaSize.height
     const w = width > screenWidth ? screenWidth : width
-    const h = height > screenHeight ? screenHeight - screen.getMenuBarHeight() : height
+    const h = height > screenHeight - screen.getMenuBarHeight() ?
+        screenHeight - screen.getMenuBarHeight() : height
     const x = Math.floor((screenWidth - w) / 2)
     const y = Math.floor((screenHeight - h + screen.getMenuBarHeight()) / 2)
     return { width: w, height: h, x, y }

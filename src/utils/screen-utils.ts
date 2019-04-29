5895d7d2d1793824a1cd43c4d587d4c4747f0f3c
nrlquaker <ustynov.vy@gmail.com>
2019-04-29 20:21:20 +0300

    Remove usage of screen.getMenuBarHeight()
    Because its deprecated

--- src/utils/screen-utils.ts
+++ src/utils/screen-utils.ts
@@ -1,12 +1,13 @@
 import { Rectangle, screen } from 'electron'
 
 export function getWindowBoundsCentered(width: number, height: number): Rectangle {
-    const screenWidth = screen.getPrimaryDisplay().workAreaSize.width
-    const screenHeight = screen.getPrimaryDisplay().workAreaSize.height
-    const w = width > screenWidth ? screenWidth : width
-    const h = height > screenHeight - screen.getMenuBarHeight() ?
-        screenHeight - screen.getMenuBarHeight() : height
-    const x = Math.floor((screenWidth - w) / 2)
-    const y = Math.floor((screenHeight - h + screen.getMenuBarHeight()) / 2)
+    const workArea = screen.getPrimaryDisplay().workArea
+    const workAreaWidth = workArea.width - workArea.x
+    const workAreaHeight = workArea.height - workArea.y
+    const w = width > workAreaWidth ? workAreaWidth : width
+    const h = height > workAreaHeight ? workAreaHeight : height
+    const x = Math.floor((workArea.x - w + workArea.width) / 2)
+    const y = Math.floor((workArea.y - h + workArea.height) / 2)
+
     return { width: w, height: h, x, y }
 }
